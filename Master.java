953231991da18e63542ba9c972d1c8f55072c9dd
rama noothan <ramakrishna.java1@gmail.com>
2018-06-07 08:00:00 +0530

    Adding master 1
    Adding master 2
    
    Adding master 3
    
    Adding krishna

---
+++ Master.java
@@ -0,0 +1,19 @@
+
+public class Master{
+
+	public static void main(String []rk){
+
+		System.out.println("I am master 1");
+	}
+	
+	public static void two(){
+		System.out.println("Master 2");
+	}
+
+	public static void three(){
+		System.out.println("Master 3"):
+	}
+
+}
+
+
